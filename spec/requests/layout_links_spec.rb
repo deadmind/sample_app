36fd2ac6e966f83c6a3a489c04fe6a7758966520
Roman Yakovenko <deadmind@ukr.net>
2011-05-14 10:23:30 +0300

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,9 @@ describe "LayoutLinks" do
     response.should have_selector("title", :content => "Help")
   end
 
+   it "Should have a Sign up page at /signup" do
+    get '/signup'
+    response.should have_selector("title", :content => "Sign up")
+  end
+
 end
